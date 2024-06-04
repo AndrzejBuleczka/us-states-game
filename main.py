9859fc036fd14683cc538b1031dc807157370d64
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-06-04 21:00:31 +0200

    train data manipulation with pandas

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import pandas
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+data_dict = data.to_dict()
+temp_list = data["temp"].to_list()
+
+max_temp = data.temp.max()
+print(data.temp[0] * 9/5 + 32)
