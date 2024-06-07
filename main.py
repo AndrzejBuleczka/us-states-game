7957f12fb28ea42e51bed77ddda92cd709b56768
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-06-07 09:55:02 +0200

    process squirrel data

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pandas
-data = pandas.read_csv("weather_data.csv")
-data_dict = data.to_dict()
-temp_list = data["temp"].to_list()
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20240607.csv")
 
-max_temp = data.temp.max()
-print(data.temp[0] * 9/5 + 32)
+gray_squirrels = data[data["Primary Fur Color"] == "Gray"]
+gray_squirrels_count = len(gray_squirrels["Primary Fur Color"])
+
+red_squirrels = data[data["Primary Fur Color"] == "Cinnamon"]
+red_squirrels_count = len(red_squirrels["Primary Fur Color"])
+
+black_squirrels = data[data["Primary Fur Color"] == "Black"]
+black_squirrels_count = len(black_squirrels["Primary Fur Color"])
+
+
+df = pandas.DataFrame(
+    {
+        "Fur Color": ["Gray", "Red", "Black"],
+        "Count": [gray_squirrels_count, red_squirrels_count, black_squirrels_count],
+    }
+)
+df.to_csv("squirrel_count.csv")
