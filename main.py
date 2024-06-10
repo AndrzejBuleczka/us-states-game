2c819abb1aa8e2f71460fe98eb4d9c31b92e9cb8
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-06-10 12:31:10 +0200

    add Exit condition and generate csv code

--- main.py
+++ main.py
@@ -15,6 +15,14 @@ score = 0
 
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{score}/50 States Correct", prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -24,16 +32,3 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
         score += 1
-
-# while len(guessed_states) < 50:
-#     answer_state = screen.textinput(title=f"{score}/50 States Correct", prompt="What's another state's name?").title()
-#     if answer_state == "Exit":
-#         break
-#     if answer_state in all_states:
-#         guessed_states.append(answer_state)
-#         t = turtle.Turtle()
-#         t.hideturtle()
-#         t.penup()
-#         state_data = data[data.state == answer_state]
-#         t.goto(int(state_data.x), int(state_data.y))
-#         t.write(answer_state)
